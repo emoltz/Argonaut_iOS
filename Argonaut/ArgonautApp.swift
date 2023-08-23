46e97afd23f82b8dd502ef77040fdbb629a19811
ethanshafranmoltz <esmoltz@gmail.com>
2023-08-23 14:18:56 -0400

    Initial Commit

---
+++ Argonaut/ArgonautApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArgonautApp.swift
+//  Argonaut
+//
+//  Created by Ethan Shafran Moltz on 8/23/23.
+//
+
+import SwiftUI
+
+@main
+struct ArgonautApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
