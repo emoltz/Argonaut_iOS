4b63dce9c11c51205eeebae0f2dd70e286003be9
ethanshafranmoltz <esmoltz@gmail.com>
2023-09-09 18:17:56 -0400

    icons added

--- Argonaut/Models/Icons.swift
+++ Argonaut/Models/Icons.swift
@@ -1,1 +1,29 @@
 import Foundation
+import SwiftUI
+
+struct IconType {
+    let name: String
+    let symbolName: String
+}
+
+let icons: [IconType] = [
+    IconType(name: "dashboard", symbolName: "gauge"),
+    IconType(name: "bell", symbolName: "bell.fill"),
+    IconType(name: "archive", symbolName: "tray.full.fill"),
+    IconType(name: "beer", symbolName: "pencil"),
+    IconType(name: "books", symbolName: "book.fill"),
+    IconType(name: "chess", symbolName: "rook"),
+    IconType(name: "key", symbolName: "key.fill"),
+    IconType(name: "cent", symbolName: "centsign.circle.fill"),
+    IconType(name: "dollar", symbolName: "dollarsign.circle.fill"),
+    IconType(name: "mail", symbolName: "envelope.fill"),
+    IconType(name: "money", symbolName: "dollarsign.circle.fill"),
+    IconType(name: "beach", symbolName: "sun.max.fill"),
+    IconType(name: "train", symbolName: "tram.fill"),
+    IconType(name: "box", symbolName: "cube.box.fill"),
+    IconType(name: "dog", symbolName: "hare"),
+    IconType(name: "home", symbolName: "house.fill"),
+    IconType(name: "medical", symbolName: "cross.fill"),
+    IconType(name: "plus", symbolName: "plus.circle.fill")
+]
+
