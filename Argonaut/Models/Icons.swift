348d1868493fcaadf43eff4baf79c411fee8c43e
ethanshafranmoltz <esmoltz@gmail.com>
2023-09-09 18:24:02 -0400

    correct symbol translation

--- Argonaut/Models/Icons.swift
+++ Argonaut/Models/Icons.swift
@@ -7,23 +7,23 @@ struct IconType {
 }
 
 let icons: [IconType] = [
-    IconType(name: "dashboard", symbolName: "gauge"),
-    IconType(name: "bell", symbolName: "bell.fill"),
+    IconType(name: "dashboard", symbolName: "gauge.with.dots.needle.50percent"),
+    IconType(name: "bell", symbolName: "bell"),
     IconType(name: "archive", symbolName: "tray.full.fill"),
-    IconType(name: "beer", symbolName: "pencil"),
+    IconType(name: "beer", symbolName: "wineglass.fill"),
     IconType(name: "books", symbolName: "book.fill"),
-    IconType(name: "chess", symbolName: "rook"),
+    IconType(name: "chess", symbolName: "rectangle.checkered"),
     IconType(name: "key", symbolName: "key.fill"),
-    IconType(name: "cent", symbolName: "centsign.circle.fill"),
-    IconType(name: "dollar", symbolName: "dollarsign.circle.fill"),
-    IconType(name: "mail", symbolName: "envelope.fill"),
-    IconType(name: "money", symbolName: "dollarsign.circle.fill"),
-    IconType(name: "beach", symbolName: "sun.max.fill"),
+    IconType(name: "cent", symbolName: "centsign"),
+    IconType(name: "dollar", symbolName: "dollarsign"),
+    IconType(name: "mail", symbolName: "envelope"),
+    IconType(name: "money", symbolName: "dollarsign"),
+    IconType(name: "beach", symbolName: "sun.max"),
     IconType(name: "train", symbolName: "tram.fill"),
-    IconType(name: "box", symbolName: "cube.box.fill"),
-    IconType(name: "dog", symbolName: "hare"),
-    IconType(name: "home", symbolName: "house.fill"),
-    IconType(name: "medical", symbolName: "cross.fill"),
-    IconType(name: "plus", symbolName: "plus.circle.fill")
+    IconType(name: "box", symbolName: "shippingbox"),
+    IconType(name: "dog", symbolName: "dog"),
+    IconType(name: "home", symbolName: "house"),
+    IconType(name: "medical", symbolName: "cross.case"),
+    IconType(name: "plus", symbolName: "plus.square")
 ]
 
